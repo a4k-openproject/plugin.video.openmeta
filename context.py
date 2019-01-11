15ae87ee4261d34219833b5aa6ebca1c1c09aca4
drinfernoo <drinfernoo@yahoo.com>
2019-01-11 12:05:16 -0800

    Fixes for context menu

--- context.py
+++ context.py
@@ -1,23 +1,16 @@
 import xbmc
 
-def get_url(stream_file):
-	return None
-
 def main():
-	stream_file = xbmc.getInfoLabel('ListItem.FileNameAndPath')
-	url = get_url(stream_file)
-	if url is None:
-		if xbmc.getInfoLabel('ListItem.Episode'):
-			url = 'plugin://plugin.video.openmeta/tv/play_by_name/%s/%s/%s/en' % (xbmc.getInfoLabel('ListItem.TVShowTitle'), xbmc.getInfoLabel('ListItem.Season'), xbmc.getInfoLabel('ListItem.Episode'))
-		elif xbmc.getInfoLabel('ListItem.IMDBNumber'):
-			url = 'plugin://plugin.video.openmeta/movies/play/imdb/%s' % xbmc.getInfoLabel('ListItem.IMDBNumber')
-		elif xbmc.getInfoLabel('ListItem.Title'):
-			url = 'plugin://plugin.video.openmeta/movies/play_by_name/%s/en' % xbmc.getInfoLabel('ListItem.Title')
-		else:
-			url = 'plugin://plugin.video.openmeta/movies/play_by_name/%s/en' % xbmc.getInfoLabel('ListItem.Label')
-		xbmc.executebuiltin('RunPlugin(%s)' % url)
+	info = xbmc.getInfoLabel
+	if info('ListItem.Episode'):
+		url = 'plugin://plugin.video.openmeta/tv/play_by_name/%s/%s/%s/en' % (info('ListItem.TVShowTitle'), info('ListItem.Season'), info('ListItem.Episode'))
+	elif info('ListItem.IMDBNumber'):
+		url = 'plugin://plugin.video.openmeta/movies/play/imdb/%s' % info('ListItem.IMDBNumber')
+	elif info('ListItem.Title'):
+		url = 'plugin://plugin.video.openmeta/movies/play_by_name/%s/en' % info('ListItem.Title')
 	else:
-		xbmc.executebuiltin('PlayMedia(%s)' % url)
-    
+		url = 'plugin://plugin.video.openmeta/movies/play_by_name/%s/en' % info('ListItem.Label')
+	xbmc.executebuiltin('RunPlugin(%s)' % url)
+
 if __name__ == '__main__':
 	main()
